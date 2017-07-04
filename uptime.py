1070a2e76900fc75ebc2e5f725cc4e1ad7453381
Everlands <everlands@users.noreply.github.com>
2017-07-04 20:39:13 +0100

    Amendments to app structure and error trapping

--- uptime.py
+++ uptime.py
@@ -23,7 +23,7 @@ monitor_colour = [ Fore.WHITE + Style.DIM, Fore.WHITE + Style.NORMAL, Fore.GREEN
 #read the config data (YAML data format)
 config = yaml.safe_load(open("uptime.yml"))
 
-# The global API key for Uptime Robot
+# Get the global API key for Uptime Robot
 ur_api_key=config['ur_api_key']
 
 
@@ -45,50 +45,73 @@ def getMonitors():
     print (Fore.CYAN + Style.BRIGHT +  'Fetching Uptime Robot Data' + Fore.RESET + Style.RESET_ALL)
     print('')
 
-    # Set up the request
-    url = 'https://api.uptimerobot.com/v2/getMonitors'
-     
-    payload = 'api_key=' + ur_api_key + '&format=json&logs=1&rnd=' + randomWord(10) #Adding random word makes sure it's not cached
-    headers = {
-        'content-type': "application/x-www-form-urlencoded",
-        'cache-control': "no-cache"
-        }
+    try:
+        # Set up the request    
+        url = 'https://api.uptimerobot.com/v2/getMonitors'
+         
+        payload = 'api_key=' + ur_api_key + '&format=json&logs=1&rnd=' + randomWord(10) #Adding random word makes sure it's not cached
+        headers = {
+            'content-type': "application/x-www-form-urlencoded",
+            'cache-control': "no-cache"
+            }
 
-    # Make the call 
-    response = requests.request('POST', url, data=payload, headers=headers)
+        # Make the call 
+        response = requests.request('POST', url, data=payload, headers=headers)
 
-    # Decode the response 
-    jsonData = json.loads(response.text)
+        # Decode the response 
+        jsonData = json.loads(response.text)
 
-    # Only want the 'monitors' json array
-    monitors = jsonData['monitors']
+        # Only want the 'monitors' json array
+        monitors = jsonData['monitors']
 
-    return monitors
-
-# Display the status of each monitor
-def showMonitors(monitors):
-    
-    # Loop through, decoding the status to appropriate colour and text
-    for monitor in monitors:
-        status = monitor['status'] 
-        print(monitor_colour[status] + monitor['friendly_name'], "is", monitor_status[status] + Fore.RESET + Style.RESET_ALL)            
+        return monitors
+    except:
+        print(Fore.RED + Style.BRIGHT + "Could not retrieve monitors. Sorry."+ Fore.RESET + Style.RESET_ALL)
+        return ""
         
+
+# Display the status of each monitor.
+# Argument is a json array of monitors
+def showMonitors(monitors):
+
+    if monitors != "":    
+        # Loop through, decoding the status to appropriate colour and text
+        for monitor in monitors:
+            status = monitor['status'] 
+            print(monitor_colour[status] + monitor['friendly_name'], "is", monitor_status[status] + Fore.RESET + Style.RESET_ALL)            
+    else:
+            print ('')
+    
     print()
 
 # Count the "down" monitors and alert!
+# Argument is a json array of monitors
 def countMonitors(monitors):
        
     downs = 0
-    
-    for monitor in monitors:
-        status = monitor['status']
-        if status=="9":
-            downs=downs+1
 
-    if downs > 0:
-        blink(10,.5)
-        
-    if downs == 1:
+    if monitors != "": 
+        for monitor in monitors:
+            status = monitor['status']
+            if status=="9":
+                downs=downs+1
+    # if there are some apps down, flash a light
+        if downs > 0:
+            blink(10,.5)
+    else:
+        # A value of -1 indicates that no count could be set.
+        downs = -1
+
+    return (downs)
+
+# Turn the count into a nice bit of human readable text.
+# Argument is an integer
+def textDownCount(downs) :       
+    if downs == -1:
+        return ('')
+    elif downs==0:
+        return ('There are no applications down')
+    elif downs == 1:
         return ('There is 1 application down')
     else:
         return ('There are ' +  str(downs) + ' applications down')   
@@ -108,8 +131,11 @@ def blink(numTimes, speed):
 
 clearScreen()    
 Monitors = getMonitors()
+
 showMonitors(Monitors)
-print (countMonitors(Monitors))
+downs = countMonitors(Monitors)
+print(textDownCount(downs))
+
 
 GPIO.cleanup()    
 print()
